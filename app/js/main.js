63772dbec99e1281dd9b9488fecb1d172de7f36a
keiichi kobayashi <k.kobayashi@logic-design.jp>
2014-10-16 13:47:32 +0900

    funky tunesをやめてstart bootstrapのsb-admib-2を導入。

--- app/js/main.js
+++ app/js/main.js
@@ -6,14 +6,24 @@ require.config({
     backbone: '../bower_components/backbone/backbone',
     marionette: '../bower_components/marionette/lib/backbone.marionette',
     text: '../bower_components/requirejs-text/text',
-    bootstrap: '../bower_components/bootstrap/dist/js/bootstrap'
+    bootstrap: '../bower_components/bootstrap/dist/js/bootstrap',
+    metisMenu: '../assets/sb-admin-2/js/plugins/metisMenu/metisMenu',
+    sb_admin_2: '../assets/sb-admin-2/js/sb-admin-2'
   },
   shim: {
     bootstrap: {
       deps: ['jquery'],
       exports: 'Bootstrap'
+    },
+    metisMenu: {
+      deps: ['bootstrap'],
+      exports: 'MetisMenu'
+    },
+    sb_admin_2: {
+      deps: ['bootstrap'],
+      exports: 'Sb_admin_2'
     }
   }
 });
 
-require(['bootstrap'], function() {});
+require(['bootstrap', 'metisMenu', 'sb_admin_2'], function() {});
--- src/coffee/main.coffee
+++ src/coffee/main.coffee
@@ -8,13 +8,23 @@ require.config
 		marionette:		'../bower_components/marionette/lib/backbone.marionette'
 		text:			'../bower_components/requirejs-text/text'
 		bootstrap:		'../bower_components/bootstrap/dist/js/bootstrap'
+		metisMenu:		'../assets/sb-admin-2/js/plugins/metisMenu/metisMenu'
+		sb_admin_2:		'../assets/sb-admin-2/js/sb-admin-2'
 	shim:
 		bootstrap:
 			deps: ['jquery']
 			exports: 'Bootstrap'
+		metisMenu:
+			deps: ['bootstrap']
+			exports: 'MetisMenu'
+		sb_admin_2:
+			deps: ['bootstrap']
+			exports: 'Sb_admin_2'
 
 require [
 	'bootstrap'
+	'metisMenu'
+	'sb_admin_2'
 ], () ->
 
 	return
