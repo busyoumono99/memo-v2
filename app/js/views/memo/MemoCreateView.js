c393d389a602d3c4fc64bc0adc24d6f4180cf28c
busyoumono99 <flyinsky1to9@gmail.com>
2014-10-29 00:21:54 +0900

    リネームしたので削除

--- app/js/views/memo/MemoCreateView.js
+++
@@ -1,8 +0,0 @@
-define(['underscore', 'marionette', 'vent', 'templates'], function(_, Marionette, vent, templates) {
-  'use strict';
-  console.log('read MemoCreateView');
-  return Marionette.ItemView.extend({
-    tagName: 'tr',
-    template: _.template(templates.memo.create)
-  });
-});
